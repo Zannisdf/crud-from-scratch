7e1e25155f910edc648bac4e73f5ef993b14a62c
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-28 16:22:25 -0300

    Refactor

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: %i[show edit update destroy]
+
   def index
     @dogs = Dog.order(:id)
   end
@@ -16,16 +18,11 @@ class DogsController < ApplicationController
     end
   end
 
-  def show
-    @dog = Dog.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @dog = Dog.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @dog = Dog.find(params[:id])
     if @dog.update(params[:dog])
       redirect_to root_path, notice: 'Dog was updated.'
     else
@@ -34,7 +31,6 @@ class DogsController < ApplicationController
   end
 
   def destroy
-    @dog = Dog.find(params[:id])
     @dog.destroy
     redirect_to root_path, notice: 'Dog was deleted.'
   end
@@ -43,5 +39,9 @@ class DogsController < ApplicationController
 
   def dog_params
     params.require(:dog).permit(:name, :age)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
   end
 end
