070b96264be700d74e3f29a4f4c765f1c6c4ff66
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-28 14:37:50 -0300

    ADD: show view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,14 +8,21 @@ class DogsController < ApplicationController
   end
 
   def create
-    dog = Dog.new(
-      name: params[:dog][:name],
-      age: params[:dog][:age]
-    )
+    dog = Dog.new(dog_params)
     if dog.save
       redirect_to root_path, notice: 'The dog was created sucessfully!'
     else
-      redirect_to root_path, notice: 'An error has ocurred, please try again.'
+      redirect_to root_path, alert: 'An error has ocurred, please try again.'
     end
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'dogs', to: 'dogs#index'
-  get 'dogs/new'
+  get 'dogs/new', as: 'new_dog'
   post 'dogs', to: 'dogs#create'
+  get 'dogs/:id', to: 'dogs#show', as: 'dog'
 
   root 'dogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
